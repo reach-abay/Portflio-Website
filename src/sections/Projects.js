14d5085177505c57a209f98731ad2a2308ac5276
Abay Raju M <67160846+reach-abay@users.noreply.github.com>
2023-10-27 00:24:03 +0530

    photo updates

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -8,10 +8,10 @@ export default function Projects() {
       id: 1,
       title: "Reminder Assistant",
       description:
-        "A mobile application for capturing events to be reminded from user speech, by using speech recognition and DistilBERT NLP model. The application picks out reminders from normal conversations.",
+        "A mobile application for capturing events to be reminded from user speech, by using speech recognition and DistilBERT NLP model. The application continuously listens to the user through the on-device mic and picks up reminders from speech.",
       image:
         "https://images.pexels.com/photos/3082341/pexels-photo-3082341.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      link: "https://google.com",
+      link: "https://github.com/reach-abay/Reminder-Assistant",
     },
     {
       id: 2,
@@ -20,7 +20,7 @@ export default function Projects() {
       "Created a demo NFT bidder and seller demo application using Google firebase and java in Android studio. The user can browse through the NFTs (dummy NFTs) and bid on the same in real-time.",
       image:
         "https://images.pexels.com/photos/17774928/pexels-photo-17774928/free-photo-of-laptop-and-water-with-lemon.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-      link: "https://google.com",
+      link: "https://github.com/reach-abay/NFT-Bidder",
     },
     {
       id: 3,
@@ -38,7 +38,7 @@ export default function Projects() {
       "Created a mobile application with the help of machine learning models, LSTM and VGG-19, that can answer questions based on live video input.",
       image:
         "https://images.pexels.com/photos/14683691/pexels-photo-14683691.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      link: "https://google.com",
+      link: "https://github.com/reach-abay/Vqa",
     },{
         id: 5,
         title: "Blogger",
@@ -46,7 +46,7 @@ export default function Projects() {
         "Blogger application with ReactJS and C#(net) where users can signup and upload blogs",
         image:
           "https://images.pexels.com/photos/2773498/pexels-photo-2773498.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        link: "https://google.com",
+        link: "https://github.com/reach-abay/Blogger",
       },
   ];
 
